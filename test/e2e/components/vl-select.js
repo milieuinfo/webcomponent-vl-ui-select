70e21d5827259c5fa7d1d43a655cb55560ea6bed
cambiph <incubusattax@gmail.com>
2020-01-27 14:59:30 +0100

    Refactor selectBy functies + toevoegen van testcases voor selectBy functies icm dressed dropdowns

--- test/e2e/components/vl-select.js
+++ test/e2e/components/vl-select.js
@@ -1,15 +1,15 @@
 const { VlElement } = require('vl-ui-core');
 const { By, Key, until } = require('selenium-webdriver');
 
-class VlSelect extends VlElement {  
+class VlSelect extends VlElement {
     async _getDressedContainer() {
         return this.findElement(By.xpath('../..'));
     }
-    
+
     async _getSelectList() {
         return (await this._getDressedContainer()).findElement(By.css('.vl-select__list'));
     }
-    
+
     async _getInput() {
         return (await this._getSelectList()).findElement(By.css('.vl-input-field'));
     }
@@ -17,23 +17,23 @@ class VlSelect extends VlElement {
     async _isOpen() {
         return this.hasClass('is-open');
     }
-    
+
     async _openDressedDropdown() {
-        if(await this._isOpen()) {
+        if (await this._isOpen()) {
             return Promise.resolve();
         }
         return (await this._getDressedContainer()).click();
     }
-    
+
     async _getSelectParent() {
         return this.findElement(By.xpath('..'));
     }
-    
+
     async _getDeleteButton() {
         const parent = await this._getSelectParent();
         return parent.findElement(By.css('button.vl-pill__close'));
     }
-    
+
     async _getOptions() {
         if (await this.isDressed()) {
             const selectList = await this._getSelectList();
@@ -42,28 +42,65 @@ class VlSelect extends VlElement {
             return this.findElements(By.css('option'));
         }
     }
-    
+
     async _mapDressedOptions(options) {
         return Promise.all(options.map(async (selectItem) => {
             const text = await selectItem.getText();
-            return {text: text, webElement: selectItem};
+            const webElement = await selectItem;
+            return { text: text, webElement: webElement };
         }));
     }
-                            
+
+    async _getWebelementMap() {
+        await this._openDressedDropdown();
+        const selectItems = await this._getOptions();
+        return this._mapDressedOptions(selectItems);
+    }
+
+    async _getValue(element) {
+        if((await this.isDressed())){
+            return element.getAttribute('data-value');
+        } else {
+            return element.getAttribute('value');
+        }
+    }
+
+    async _mapValues(options) {
+        return Promise.all(options.map(async (option) => {
+            const value = await this._getValue(option);
+            return { webElement: option, value: value }
+        }));
+    }
+
+    async _mapVisibleText(options) {
+        return Promise.all(options.map(async (option) => {
+            const textContent = await option.getAttribute('textContent');
+            const visibleText = textContent.replace(/\s+/g, ' ').trim();
+            return { webElement: option, visibleText: visibleText }
+        }));
+    }
+
+    async _clickOption(option) {
+        if ((await this.isDressed())) {
+            await this._openDressedDropdown();
+        }
+        return option.webElement.click();
+    }
+
     async isDressed() {
         return this.hasAttribute('data-vl-select-dressed');
     }
-    
+
     async values() {
         const options = await this._getOptions();
         return Promise.all(options.map(o => o.getAttribute('value')));
     }
-    
+
     async hasValue(value) {
         const values = await this.values();
         return values.includes(value);
     }
-    
+
     async getAllVisibleText() {
         const options = await this._getOptions();
         return Promise.all(options.map(o => o.getAttribute('textContent')));
@@ -75,41 +112,27 @@ class VlSelect extends VlElement {
     }
 
     async selectByValue(value) {
-        if(await !this.hasValue(value)) {
+        if (await !this.hasValue(value)) {
             return Promise.reject('Value ' + value + ' niet gevonden in select!');
         }
-        if((await this.isDressed())) {
-            await this._openDressedDropdown();
-            const selectItems = await this._getOptions();
-            const map = await this._mapDressedOptions(selectItems);
-
-            const webElementArray = map.filter(i => i.text == value);
-            return webElementArray[0].webElement.click();
-        } else {
-            await this._openDressedDropdown();
-            return (await this.findElement(By.css('option[value="' + value + '"]'))).click();
-        }
+        const options = await this._getOptions();
+        const map = await this._mapValues(options);
+        const option = await map.filter(m => m.value == value)[0];
+        return this._clickOption(option);
     }
 
     async selectByVisibleText(visibleText) {
-        if(await !this.hasVisibleText(visibleText)) {
+        if (await !this.hasVisibleText(visibleText)) {
             return Promise.reject('Text ' + visibleText + ' niet gevonden in select!');
         }
-        if((await this.isDressed())) {
-            await this._openDressedDropdown();
-            const selectItems = await this._getOptions();
-            const map = await this._mapDressedOptions(selectItems);
-
-            const webElementArray = map.filter(i => i.text == visibleText);
-            return webElementArray[0].webElement.click();
-        } else {
-            await this._openDressedDropdown();
-            return (await this.findElement(By.xpath('//option[text() = "' + visibleText + '"]'))).click();
-        }
+        const options = await this._getOptions();
+        const map = await this._mapVisibleText(options);
+        const option = await map.filter(m => m.visibleText == visibleText)[0];
+        return this._clickOption(option);
     }
 
     async selectByIndex(index) {
-        if((await this.isDressed())) {
+        if ((await this.isDressed())) {
             await this._openDressedDropdown();
             const selectItems = await this._getOptions();
             return selectItems[index].click();
@@ -120,9 +143,8 @@ class VlSelect extends VlElement {
         }
     }
 
-
     async search(searchText) {
-        if(this.hasValue(searchText) == false) {
+        if (this.hasValue(searchText) == false) {
             return Promise.reject('Waarde ' + searchText + ' niet gevonden in de dropdown!');
         }
         await this._openDressedDropdown();
@@ -132,7 +154,7 @@ class VlSelect extends VlElement {
     }
 
     async deleteValue(value) {
-        if(this.hasValue(value)) {
+        if (this.hasValue(value)) {
             return (await this._getDeleteButton()).click();
         }
         return Promise.reject('Waarde ' + value + ' niet gevonden in de dropdown!')
--- test/e2e/select.test.js
+++ test/e2e/select.test.js
@@ -97,7 +97,7 @@ describe('vl-select', async () => {
         await assert.eventually.equal(select.getSelectedValue(), 'Duitsland');
         await vlSelectPage.verwijder();
         await assert.eventually.isEmpty(select.getSelectedValue());
-    });
+     });
 
     it('ik kan een optie selecteren via text', async () => {
         const select = await vlSelectPage.getDefaultSelect();
@@ -106,8 +106,36 @@ describe('vl-select', async () => {
         assert.isTrue(text == 'Frankrijk');
     });
 
+    it('ik kan een optie selecteren via text in een dressed dropdown', async () => {
+        const select = await vlSelectPage.getSearchSelect();
+        await select.selectByVisibleText('Duitsland');
+        const text = await select.getSelectedValue();
+        assert.isTrue(text == 'Duitsland');
+    })
+
+    it('ik kan een optie selecteren via value', async () => {
+        const select = await vlSelectPage.getDefaultSelect();
+        await select.selectByValue('Duitsland');
+        const text = await select.getSelectedValue();
+        assert.isTrue(text == 'Duitsland');
+    });
+
+    it('ik kan een optie selecteren via value in een dressed dropdown', async () => {
+        const select = await vlSelectPage.getSearchSelect();
+        await select.selectByValue('Frankrijk');
+        const text = await select.getSelectedValue();
+        assert.isTrue(text == 'Frankrijk');
+    });
+
     it('ik kan een optie selecteren via index', async () => {
         const select = await vlSelectPage.getDefaultSelect();
+        await select.selectByIndex(2);
+        const text = await select.getSelectedValue();
+        assert.isTrue(text == 'Frankrijk');
+    });
+
+    it('ik kan een optie selecteren via index in een dressed dropdown', async () => {
+        const select = await vlSelectPage.getSearchSelect();
         await select.selectByIndex(1);
         const text = await select.getSelectedValue();
         assert.isTrue(text == 'Duitsland');
